648c77f74fd1ea492166b420d4e914611f6aab89
Noel Osiro <noel.osiro@azubiafrica.org>
2024-06-06 19:00:30 +0300

    start

---
+++ src/app/page.tsx
@@ -0,0 +1,59 @@
+import { Container } from "@/components/Container";
+import { Hero } from "@/components/Hero";
+import { SectionTitle } from "@/components/SectionTitle";
+import { Benefits } from "@/components/Benefits";
+import { Video } from "@/components/Video";
+import { Testimonials } from "@/components/Testimonials";
+import { Faq } from "@/components/Faq";
+import { Cta } from "@/components/Cta";
+
+import { benefitOne, benefitTwo } from "@/components/data";
+export default function Home() {
+  return (
+    <Container>
+      <Hero />
+      <SectionTitle
+        preTitle="What we do"
+        title="Dedication to catalyzing transformative attitudes, and equipping individuals"
+      >
+        Torch Initiatives is dedicated to social responsibility, advocating for positive youth
+        attitudes and aptitudes while championing community sustainability. We emphasize the
+        proper utilization of community resources, nurturing positive cultures, and responsible
+        stewardship.
+      </SectionTitle>
+
+      <Benefits data={benefitOne} />
+      <Benefits imgPos="right" data={benefitTwo} />
+
+      <SectionTitle
+        preTitle="Our values"
+        title="Be a Volunteer Our Community"
+      >
+        Our values encompass a commitment to sustainability, 
+        inclusivity, integrity, divine guidance, and plant-based advocacy, 
+        prioritizing the welfare of humans and animals, 
+        while investing in the potential of young people as beacons of hope for the future.
+      </SectionTitle>
+
+      <Video videoId="fZ0D0cnR88E" />
+
+      <SectionTitle
+        preTitle="Testimonials"
+        title="Here's what our customers said"
+      >
+        Testimonials is a great way to increase the brand trust and awareness.
+        Use this section to highlight your popular customers.
+      </SectionTitle>
+
+      <Testimonials />
+
+      <SectionTitle preTitle="FAQ" title="Frequently Asked Questions">
+        Answer your customers possible questions here, it will increase the
+        conversion rate as well as support or chat requests.
+      </SectionTitle>
+
+      <Faq />
+      <Cta />
+    </Container>
+  );
+}
---
+++ src/components/data.js
@@ -0,0 +1,64 @@
+import {
+  FaceSmileIcon,
+  ChartBarSquareIcon,
+  CursorArrowRaysIcon,
+  DevicePhoneMobileIcon,
+  AdjustmentsHorizontalIcon,
+  SunIcon,
+  BookOpenIcon,
+  UsersIcon,
+  ShoppingBagIcon,
+} from "@heroicons/react/24/solid";
+
+import benefitOneImg from "../../public/img/youth.jpg";
+import benefitTwoImg from "../../public/img/benefit-two.png";
+import { CloudIcon } from "@heroicons/react/24/solid";
+
+const benefitOne = {
+  title: "Empowering Youth",
+  desc: "We focus on instilling positive attitudes and 21st-century employability skills in young people, preparing them to thrive in a rapidly evolving world.",
+  image: benefitOneImg,
+  bullets: [
+    {
+      title: "Promote Education",
+      desc: "Encourage and support the pursuit of education to empower youth with knowledge and skills.",
+      icon: <BookOpenIcon/>,
+    },
+    {
+      title: "Foster Leadership",
+      desc: "Provide opportunities for leadership roles to instill confidence and decision-making abilities.",
+      icon: <FaceSmileIcon />,
+    },
+    {
+      title: "Encourage Community Involvement",
+      desc: "Engage youth in community service to foster a sense of responsibility and belonging.",
+      icon: <UsersIcon/>,
+    },
+  ],
+};
+
+const benefitTwo = {
+  title: "Building Sustainable Communities",
+  desc: "Our community empowerment initiatives center on cultivating sustainable attitudes.",
+  image: benefitTwoImg,
+  bullets: [
+    {
+      title: "Promote Green Living",
+      desc: "Encourage the adoption of eco-friendly practices and sustainable living.",
+      icon: <CloudIcon />,
+    },
+    {
+      title: "Support Local Economies",
+      desc: "Promote local businesses and farmers to boost the local economy and reduce carbon footprint.",
+      icon: <ShoppingBagIcon/>,
+    },
+    {
+      title: "Encourage Community Participation",
+      desc: "Foster a sense of community through participation in local events and initiatives.",
+      icon: <UsersIcon />,
+    },
+  ],
+};
+
+
+export {benefitOne, benefitTwo};
