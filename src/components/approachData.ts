1f070930bf3b6b2b929b6d068a6a40b3b515613d
Noel Osiro <noel.osiro@azubiafrica.org>
2024-06-12 06:58:13 +0300

    approach

--- src/components/approachData.ts
+++ src/components/approachData.ts
@@ -2,18 +2,18 @@ import { ApproachProps } from "./Approaches";
 
 export const approaches: ApproachProps[] = [
   {
-    imgSrc: '/img/empower4.jpg',
-    title: "Plant-Based Diet Education Program",
-    description: "Torch Initiatives introduced and promoted plant-based diets in schools, addressing health, environmental concerns, and animal welfare. Engaging 1,500 students, 50 teachers, and 10 kitchen staff at Naramat School in Kajiado County and Joy Schools in Nairobi County, the program transformed dietary habits and fostered a culture of sustainability. Age-appropriate sessions for grades 4-8 ensured relevance and engagement.",
-    buttonText: "Learn More",
-    tag: "Education",
-  },
-  {
     imgSrc: '/img/empower2.jpeg',
     title: 'Empowering youth',
     description: 'We aim to equip young people with positive attitudes and 21st-century employability skills, preparing them to succeed in a rapidly changing world. Our targeted programs develop the mindset and skills needed to navigate modern workforce challenges, fostering confident and adaptable individuals ready for success.',
     buttonText: 'Follow',
     tag: 'Youth',
+  },
+  {
+    imgSrc: '/img/empower4.jpeg',
+    title: "Plant-Based Diet Education Program",
+    description: "Torch Initiatives introduced and promoted plant-based diets in schools, addressing health, environmental concerns, and animal welfare. Engaging 1,500 students, 50 teachers, and 10 kitchen staff at Naramat School in Kajiado County and Joy Schools in Nairobi County, the program transformed dietary habits and fostered a culture of sustainability. Age-appropriate sessions for grades 4-8 ensured relevance and engagement.",
+    buttonText: "Learn More",
+    tag: "Education",
   },
   {
     imgSrc: '/img/empower3.jpg',
