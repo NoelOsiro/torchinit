16ad3e8eb12bf16f38aa80a6f2697af934696fe8
Noel Osiro <noel.osiro@azubiafrica.org>
2024-06-12 06:51:35 +0300

    approach

--- src/components/approachData.ts
+++ src/components/approachData.ts
@@ -2,7 +2,14 @@ import { ApproachProps } from "./Approaches";
 
 export const approaches: ApproachProps[] = [
   {
-    imgSrc: '/img/empower4.jpeg',
+    imgSrc: '/img/empower4.jpg',
+    title: "Plant-Based Diet Education Program",
+    description: "Torch Initiatives introduced and promoted plant-based diets in schools, addressing health, environmental concerns, and animal welfare. Engaging 1,500 students, 50 teachers, and 10 kitchen staff at Naramat School in Kajiado County and Joy Schools in Nairobi County, the program transformed dietary habits and fostered a culture of sustainability. Age-appropriate sessions for grades 4-8 ensured relevance and engagement.",
+    buttonText: "Learn More",
+    tag: "Education",
+  },
+  {
+    imgSrc: '/img/empower2.jpeg',
     title: 'Empowering youth',
     description: 'We aim to equip young people with positive attitudes and 21st-century employability skills, preparing them to succeed in a rapidly changing world. Our targeted programs develop the mindset and skills needed to navigate modern workforce challenges, fostering confident and adaptable individuals ready for success.',
     buttonText: 'Follow',
@@ -15,12 +22,5 @@ export const approaches: ApproachProps[] = [
     description: 'Our community empowerment initiatives focus on sustainable attitudes, utilizing local resources for development, raising awareness for better livelihoods, and empowering school children through cultural awareness. We also advocate for veganism, promoting environmental and ethical benefits and fostering empathy. Using innovative and inclusive methods, we strive to make a lasting positive impact on individuals and communities.',
     buttonText: 'Discover',
     tag: 'Commuinty',
-  },
-  {
-    imgSrc: '/img/empower2.jpg',
-    title: "Plant-Based Diet Education Program",
-    description: "Torch Initiatives introduced and promoted plant-based diets in schools, addressing health, environmental concerns, and animal welfare. Engaging 1,500 students, 50 teachers, and 10 kitchen staff at Naramat School in Kajiado County and Joy Schools in Nairobi County, the program transformed dietary habits and fostered a culture of sustainability. Age-appropriate sessions for grades 4-8 ensured relevance and engagement.",
-    buttonText: "Learn More",
-    tag: "Education",
-  },
+  }
 ];
