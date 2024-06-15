b085f72fac698654beb05f097f9111182553b5d3
NoelOsiro <noel.osiro@azubiafrica.org>
2024-06-15 18:51:19 +0300

    favico

--- src/components/Approaches.tsx
+++ src/components/Approaches.tsx
@@ -9,6 +9,7 @@ export interface ApproachProps {
     buttonText: string;
     tag: string;
     imgPos?: "left" | "right";
+    caption: string;
 
 };
 
@@ -31,6 +32,7 @@ export const Approaches = (props: ApproachesProps) => {
                         buttonText={item.buttonText}
                         tag={item.tag}
                         imgPos={item.imgPos}
+                        caption={item.caption}
                     />
                 ))}
             </div>
@@ -41,15 +43,18 @@ export const Approaches = (props: ApproachesProps) => {
 
 
 
-const Approach = ({ imgSrc, title, tag, description, buttonText, imgPos = "left" }: ApproachProps) => {
+const Approach = ({ imgSrc, title, tag, description, caption, buttonText, imgPos = "left" }: ApproachProps) => {
     return (
         <div className={`flex flex-col overflow-hidden rounded-md shadow-md ${imgPos === "right" ? "lg:flex-row-reverse" : "lg:flex-row"}`}>
-            <Image
-                width={640}
-                height={480}
-                src={imgSrc}
-                alt={title}
-                className="h-96 w-3/5 dark:bg-gray-500 aspect-video" />
+            <figure className="relative dark:bg-gray-500 aspect-video hover:scale-110">
+                <Image
+                    width={640}
+                    height={480}
+                    src={imgSrc}
+                    alt={title}
+                    className="object-cover rounded-lg" />
+                <figcaption className="absolute bottom-4 w-full text-center mt-2 text-lg text-white font-bold transition-transform duration-500 hover:scale-110">{caption}</figcaption>
+            </figure>
             <div className="flex flex-col justify-center flex-1 p-8">
                 <span className="text-xs uppercase dark:text-gray-400">{tag}</span>
                 <h3 className="text-3xl font-bold dark:text-green-400">{title}</h3>
--- src/components/approachData.ts
+++ src/components/approachData.ts
@@ -7,6 +7,7 @@ export const approaches: ApproachProps[] = [
     description: 'We aim to equip young people with positive attitudes and 21st-century employability skills, preparing them to succeed in a rapidly changing world. Our targeted programs develop the mindset and skills needed to navigate modern workforce challenges, fostering confident and adaptable individuals ready for success.',
     buttonText: 'Follow',
     tag: 'Youth',
+    caption: 'Empowering',
   },
   {
     imgSrc: '/img/empower4.jpeg',
@@ -14,6 +15,7 @@ export const approaches: ApproachProps[] = [
     description: "Torch Initiatives introduced and promoted plant-based diets in schools, addressing health, environmental concerns, and animal welfare. Engaging 1,500 students, 50 teachers, and 10 kitchen staff at Naramat School in Kajiado County and Joy Schools in Nairobi County, the program transformed dietary habits and fostered a culture of sustainability. Age-appropriate sessions for grades 4-8 ensured relevance and engagement.",
     buttonText: "Learn More",
     tag: "Education",
+    caption: 'Empowering',
   },
   {
     imgSrc: '/img/empower3.jpg',
@@ -22,5 +24,6 @@ export const approaches: ApproachProps[] = [
     description: 'Our community empowerment initiatives focus on sustainable attitudes, utilizing local resources for development, raising awareness for better livelihoods, and empowering school children through cultural awareness. We also advocate for veganism, promoting environmental and ethical benefits and fostering empathy. Using innovative and inclusive methods, we strive to make a lasting positive impact on individuals and communities.',
     buttonText: 'Discover',
     tag: 'Commuinty',
+    caption: 'Empowering',
   }
 ];
