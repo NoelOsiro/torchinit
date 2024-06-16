cdb0015577594e9dd372d190ca4163a6947a8300
NoelOsiro <noel.osiro@azubiafrica.org>
2024-06-16 04:13:13 +0300

    eail

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,50 +1,60 @@
-import { NextResponse, NextRequest } from 'next/server'
-const nodemailer = require('nodemailer');
+import { NextResponse } from 'next/server'
+import nodemailer from 'nodemailer';
 
 // Handles POST requests to /api
+const username = process.env.NEXT_PUBLIC_EMAIL_USERNAME;
+const password = process.env.NEXT_EMAIL_PASSWORD;
+const myEmail = process.env.NEXT_PUBLIC_PERSONAL_EMAIL;
 
+export async function POST(request: Request) {
 
-export async function POST(request:NextRequest) {
-
-    const username = process.env.NEXT_PUBLIC_BURNER_USERNAME;
-    const password = process.env.NEXT_PUBLIC_BURNER_PASSWORD;
-    const myEmail = process.env.NEXT_PUBLIC_PERSONAL_EMAIL;
-
-
-    console.log("dealing with request")
-    const formData = await request.formData()
-    const name = formData.get('name')
-    const email = formData.get('email')
-    const message = formData.get('message')
+    //get body of request
+    const formData = await request.json();
+    const { name, email, message } = formData;
 
 
     // create transporter object
     const transporter = nodemailer.createTransport({
-        host: "smtp-mail.outlook.com",
-        port: 587,
-        tls: {
-            ciphers: "SSLv3",
-            rejectUnauthorized: false,
-        },
-
+        host: "5.135.108.190",
+        port: 465,
+        tls: { rejectUnauthorized: false },
         auth: {
-
             user: username,
             pass: password
         }
-    });
+    } as nodemailer.TransportOptions);
 
     try {
 
         const mail = await transporter.sendMail({
-            from: username,
+            from: email,
             to: myEmail,
             replyTo: email,
             subject: `Website activity from ${email}`,
             html: `
-            <p>Name: ${name} </p>
-            <p>Email: ${email} </p>
-            <p>Message: ${message} </p>
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <style>
+                    body {font-family: Arial, Helvetica, sans-serif;}
+                    .container {padding: 20px;}
+                    .header {background-color: #f8f8f8; padding: 10px; text-align: center;}
+                    .content {margin-top: 20px;}
+                </style>
+            </head>
+            <body>
+                <div class="container">
+                    <div class="header">
+                        <h2>Website Activity</h2>
+                    </div>
+                    <div class="content">
+                        <p><strong>Name:</strong> ${name}</p>
+                        <p><strong>Email:</strong> ${email}</p>
+                        <p><strong>Message:</strong> ${message}</p>
+                    </div>
+                </div>
+            </body>
+            </html>
             `,
         })
 
