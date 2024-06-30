5f176ac8fec05f44dddfcf98abc833e147806471
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-06-30 10:22:59 +0000

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -92,14 +92,7 @@ export function Footer() {
             </div>
           </div>
         </div>
-
-        <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
-          Copyright © {new Date().getFullYear()}. Made with ♥ by{" "}
-          <a href="https://github.com/NoelOsiro" target="_blank" rel="noopener">
-            NoelOsiro
-          </a>
-        </div>
-      </Container>
+        </Container>
     </div>
   );
 }
